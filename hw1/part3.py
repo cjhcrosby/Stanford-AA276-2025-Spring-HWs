1ffaf9bf96f6565512c531ba8100ad1ae564c24a
cjhcrosby <cjcrosby@stanford.edu>
2025-04-22 22:14:47 -0700

    Update part3.py

--- hw1/part3.py
+++ hw1/part3.py
@@ -131,8 +131,8 @@ def plot_and_eval_xts(fig, ax, x0, u_ref_fn, h_fn, dhdx_fn, gamma, lmbda, nt, dt
         positions = xts_np[i, :, :2]
         
         color = 'blue' if initially_safe[i] else 'red'
-        ax.plot(positions[:, 0], positions[:, 1], color=color, alpha=0.5)
-        ax.scatter(positions[0, 0], positions[0, 1], color=color, marker='o')
+        ax.plot(positions[:, 0], positions[:, 1], color=color, alpha=0.3)
+        ax.scatter(positions[0, 0], positions[0, 1], color=color, marker='o', alpha = 0.3)
 
     for i in range(batch_size):
         if initially_safe[i]:
