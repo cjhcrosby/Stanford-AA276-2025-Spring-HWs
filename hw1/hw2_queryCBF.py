61d3f19dfff125ce50f58cd7f1d13606569d5a5c
cjhcrosby <cjcrosby@stanford.edu>
2025-05-02 09:19:20 -0700

    Update hw2_queryCBF.py

--- hw1/hw2_queryCBF.py
+++ hw1/hw2_queryCBF.py
@@ -39,3 +39,13 @@ class NeuralCBF:
         """
         return -self.model.V_with_jacobian(x)[1].squeeze(1)
     
+# create a neuralcbf object
+neuralcbf = NeuralCBF()
+# create a random state
+x = torch.rand(1, 13)
+# get the h values and gradients
+h_values = neuralcbf.values(x)
+h_gradients = neuralcbf.gradients(x)
+# print the shapes
+print('h_values shape:', h_values.shape)
+print('h_gradients shape:', h_gradients.shape)
