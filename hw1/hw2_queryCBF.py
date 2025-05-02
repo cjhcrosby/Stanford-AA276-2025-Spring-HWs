975baf4daa77b1f84df347bacb6f2e42fea8d450
cjhcrosby <cjcrosby@stanford.edu>
2025-05-02 09:35:39 -0700

    Update hw2_queryCBF.py

--- hw1/hw2_queryCBF.py
+++ hw1/hw2_queryCBF.py
@@ -54,15 +54,18 @@ upper = torch.tensor([p_u,p_u,p_u,q_u,q_u,q_u,q_u,v_u,v_u,v_u,w_u,w_u,w_u])
 lower = torch.tensor([p_l,p_l,p_l,q_l,q_l,q_l,q_l,v_l,v_l,v_l,w_l,w_l,w_l])
 
 # create a large batch of random states to randomly sample from
+h_values = []
 batch_size = 1000
-x = torch.rand(batch_size, 13)*(upper-lower)+lower
+for i in range(100):
+    x = torch.rand(batch_size, 13)*(upper-lower)+lower
+    h_values.append(neuralcbf.values(x))
+        
+h_values = torch.cat(h_values, dim=0)
 # get the h values and gradients
-h_values = neuralcbf.values(x)
 h_gradients = neuralcbf.gradients(x)
 # print the shapes of the h values and gradients
 print(f'h values shape: {h_values.shape}')
-print(f'h gradients shape: {h_gradients.shape}')
 # print h values above 0
-print(f'h values above 0: {h_values[h_values > 0].shape[0]}')
-# print h values below 0 divided by total samples
-print(f'h values below 0: {h_values[h_values < 0].shape[0] / batch_size}')
+print(f'h values above 0: {h_values[h_values >= 0].shape[0]}')
+# print h values above 0 divided by total samples
+print(f'h values below 0: {h_values[h_values >= 0].shape[0] / h_values.shape[0]}')
